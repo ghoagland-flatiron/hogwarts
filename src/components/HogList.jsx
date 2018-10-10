a1bdb39b195fc521a0e23c04cdfffbcf8de222c0
ghoagland-flatiron <georgina.hoagland@flatironschool.com>
2018-10-10 10:15:12 -0400

    progress

---
+++ src/components/HogList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class HogList extends Component {
+  makeHogCards(hogsArr) {
+    return hogsArr.map(hog => {
+
+    })
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default HogList
